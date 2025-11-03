6ed79afe83b9fd9c73704ced84d255b93414d5d0
Carlos-GH23 <20223tn124@utez.edu.mx>
2025-11-03 01:00:31 -0600

    Correcciones De Versiones

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Offline-only (Only-Cache) Service Worker */
-const CACHE = 'only-cache-v3';
+const CACHE = 'only-cache-v4';
 
 // Lista de recursos estáticos a precachear (relativos al scope)
 const ASSETS = [
